239c601157b467232620645eb5afb2a7e8095001
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2019-10-07 00:14:16 +0200

    Update chevron down icon

--- public/js/app.js
+++ public/js/app.js
@@ -3962,17 +3962,20 @@ var render = function() {
         _c(
           "svg",
           {
-            staticClass: "ml-1 h-6 w-6 fill-current",
-            attrs: { xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 24 24" }
+            staticClass: "ml-1",
+            attrs: {
+              xmlns: "http://www.w3.org/2000/svg",
+              width: "16",
+              height: "16",
+              viewBox: "0 0 24 24",
+              fill: "none",
+              stroke: "currentColor",
+              "stroke-width": "2",
+              "stroke-linecap": "round",
+              "stroke-linejoin": "round"
+            }
           },
-          [
-            _c("path", {
-              attrs: {
-                d:
-                  "M15.3 9.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z"
-              }
-            })
-          ]
+          [_c("polyline", { attrs: { points: "6 9 12 15 18 9" } })]
         )
       ]
     ),
--- resources/js/components/AccountDropdown.vue
+++ resources/js/components/AccountDropdown.vue
@@ -4,8 +4,8 @@
         <button type="button" class="relative z-10 flex items-center focus:outline-none" @click="isOpen = !isOpen">
             <img src="https://github.com/adamwathan.png" alt="Adam Wathan's avatar" class="rounded-full h-8 w-8">
             <span class="ml-2">Adam Wathan</span>
-            <svg class="ml-1 h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path d="M15.3 9.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z"/>
+            <svg xmlns="http://www.w3.org/2000/svg" class="ml-1" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <polyline points="6 9 12 15 18 9"></polyline>
             </svg>
         </button>
         <div v-if="isOpen" class="absolute right-0 left-auto bg-white border shadow py-2 rounded">
