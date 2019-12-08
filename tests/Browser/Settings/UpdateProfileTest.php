53b530e9af7eff7b8418fdabfa90df9c1c167553
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2019-12-08 21:01:37 +0100

    Debug

--- tests/Browser/Settings/UpdateProfileTest.php
+++ tests/Browser/Settings/UpdateProfileTest.php
@@ -15,6 +15,10 @@ class UpdateProfileTest extends DuskTestCase
     /** @test */
     public function user_can_update_own_profile()
     {
+        dump(config('app'));
+        dump(config('database'));
+        dump(config('cashier'));
+
         $user = factory(User::class)->create();
 
         $this->browse(function (Browser $browser) use ($user) {
